160df82d6d234a655cc841c63e87d4efcfd2f7e4
fer <fer@example.com>
2026-02-19 14:43:59 -0600

    falta diseño

---
+++ desafio1ferylisi/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace desafio1ferylisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmontoTotal = new System.Windows.Forms.TextBox();
+            this.txtprecioProducto = new System.Windows.Forms.TextBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbPrecioTotal = new System.Windows.Forms.Label();
+            this.Descuento = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(84, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el monto total de la compra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(316, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 1;
+            // 
+            // txtmontoTotal
+            // 
+            this.txtmontoTotal.Location = new System.Drawing.Point(395, 193);
+            this.txtmontoTotal.Multiline = true;
+            this.txtmontoTotal.Name = "txtmontoTotal";
+            this.txtmontoTotal.Size = new System.Drawing.Size(114, 31);
+            this.txtmontoTotal.TabIndex = 2;
+            // 
+            // txtprecioProducto
+            // 
+            this.txtprecioProducto.Location = new System.Drawing.Point(395, 138);
+            this.txtprecioProducto.Multiline = true;
+            this.txtprecioProducto.Name = "txtprecioProducto";
+            this.txtprecioProducto.Size = new System.Drawing.Size(114, 31);
+            this.txtprecioProducto.TabIndex = 3;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.DropDownHeight = 168;
+            this.cmbCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.IntegralHeight = false;
+            this.cmbCategoria.ItemHeight = 18;
+            this.cmbCategoria.Location = new System.Drawing.Point(612, 184);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(140, 26);
+            this.cmbCategoria.TabIndex = 4;
+            this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.cmbCategoria_SelectedIndexChanged);
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.Color.SteelBlue;
+            this.btncalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btncalcular.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btncalcular.Location = new System.Drawing.Point(353, 306);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(97, 38);
+            this.btncalcular.TabIndex = 5;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(591, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seleccione una categoria";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.Location = new System.Drawing.Point(84, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ingrese el precio del producto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.SteelBlue;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 15F);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(256, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(280, 26);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Calculadora de descuentos";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lbPrecioTotal
+            // 
+            this.lbPrecioTotal.AutoSize = true;
+            this.lbPrecioTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPrecioTotal.Location = new System.Drawing.Point(131, 342);
+            this.lbPrecioTotal.Name = "lbPrecioTotal";
+            this.lbPrecioTotal.Size = new System.Drawing.Size(0, 20);
+            this.lbPrecioTotal.TabIndex = 9;
+            this.lbPrecioTotal.Click += new System.EventHandler(this.lbPrecioTotal_Click);
+            // 
+            // Descuento
+            // 
+            this.Descuento.AutoSize = true;
+            this.Descuento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Descuento.Location = new System.Drawing.Point(132, 388);
+            this.Descuento.Name = "Descuento";
+            this.Descuento.Size = new System.Drawing.Size(0, 20);
+            this.Descuento.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SteelBlue;
+            this.textBox1.Location = new System.Drawing.Point(-12, -2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(838, 91);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Descuento);
+            this.Controls.Add(this.lbPrecioTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.txtprecioProducto);
+            this.Controls.Add(this.txtmontoTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Calculadora de descuentos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmontoTotal;
+        private System.Windows.Forms.TextBox txtprecioProducto;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbPrecioTotal;
+        private System.Windows.Forms.Label Descuento;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ desafio1ferylisi/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio1ferylisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCategoria.Items.Add("Electrónica");
+            cmbCategoria.Items.Add("Ropa");
+            cmbCategoria.Items.Add("Alimentos");
+            cmbCategoria.Items.Add("Hogar");
+        }
+
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double precioProducto;
+            double montoTotal;
+            double descuento = 0;
+            double precioFinal = 0;
+
+            // 🔹 VALIDACIONES
+
+            // Validar que el precio del producto sea número válido
+            if (!double.TryParse(txtprecioProducto.Text, out precioProducto) || precioProducto <= 0)
+            {
+                MessageBox.Show("Ingrese un precio de producto válido (número positivo).", "Error");
+                return;
+            }
+
+            // Validar que el monto total sea número válido
+            if (!double.TryParse(txtmontoTotal.Text, out montoTotal) || montoTotal <= 0)
+            {
+                MessageBox.Show("Ingrese un monto total válido (número positivo).", "Error");
+                return;
+            }
+
+            // Validar que haya seleccionado una categoría
+            if (cmbCategoria.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione una categoría.", "Error");
+                return;
+            }
+
+            string categoria = cmbCategoria.SelectedItem.ToString();
+
+            // 🔹 SWITCH PARA LAS CATEGORÍAS
+            switch (categoria)
+            {
+                case "Electrónica":
+                    if (montoTotal > 500)
+                    {
+                        descuento = montoTotal * 0.10;
+                    }
+                    break;
+
+                case "Ropa":
+                    if (montoTotal > 300)
+                    {
+                        descuento = montoTotal * 0.15;
+                    }
+                    break;
+
+                case "Alimentos":
+                    if (montoTotal > 200)
+                    {
+                        descuento = montoTotal * 0.05;
+                    }
+                    break;
+
+                case "Hogar":
+                    if (montoTotal > 400)
+                    {
+                        descuento = montoTotal * 0.20;
+                    }
+                    break;
+            }
+
+            // 🔹 IF-ELSE PARA MENSAJE DE DESCUENTO
+            if (descuento > 0)
+            {
+                precioFinal = montoTotal - descuento;
+                Descuento.Text = $"Se aplicó un descuento de ${descuento:F2}";
+            }
+            else
+            {
+                precioFinal = montoTotal;
+                Descuento.Text = "No se aplicó ningún descuento.";
+            }
+
+            // Mostrar precio final
+            lbPrecioTotal.Text = $"Precio final a pagar: ${precioFinal:F2}";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbPrecioTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
